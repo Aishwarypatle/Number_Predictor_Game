226018b8f5507445a38c14b44f63ff3b14f49e7c
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2022-12-16 13:20:05 +0530

    code added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
+
+  let num = Math.floor(Math.random() *100);
+
+  let num2 = 0; 
+  
+  
+  const checkNum =()=>
+  {
+    if(num2 === num )
+    {
+      console.log('right')
+    }
+    else if (num2!== num && num2 < num)
+    {
+      console.log( 'Your num is small')
+    }
+
+    else if(num2!==num && num2>num)
+    {
+      console.log('your num is greater')
+    }
+
+  }
+
+
+  const handleChange  =(event)=>
+    {
+      num2 = event.target.value;
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className="d-flex align-items-center justify-content-center vh-100 vw-100">
+        <div className="bg-dark card border border-dark d-grid align-items-center justify-content-center vh-50 vw-50 backg shadow p-3 mb-5 bg-white rounded ">
+              <div className="header">
+              <h5>Guess the Number Between 1 to 100</h5>
+              </div>
+              <div className="d-flex justify-content-center m-3">
+                <input
+                className="h-70 w-25 "
+                onChange={handleChange}
+                >
+                </input>
+              </div>
+          
+              <div className=" d-flex">
+                <button className="btn btn-success m-2 p-2 w-50" onClick={checkNum} > Play </button>
+                <button className="btn btn-danger m-2 p-2 w-50">Reset</button>
+
+              </div>
+
+
+                            
+            
+        </div>
+
+      </div>
+      
+      </>
   );
 }
 
